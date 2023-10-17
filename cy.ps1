292bdccb9c9154a2df2bfba4444afd79094e6071
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-17 13:02:24 -0400

    Update cy.ps1

--- cy.ps1
+++ cy.ps1
@@ -1,17 +1,17 @@
-$cyCount = $args.Count-1
+$cyEnd = $args.Count-1
 if ( $args[0] -eq 'ntlm' ) {
 	if ( $args[1] -eq 'run' ) {
 		if ( $args[2] -eq $null ) {
 			cmd /c "npx cypress-ntlm run --browser chrome"
 		} else {
-			cmd /c "npx cypress-ntlm run --browser chrome $($args[2..$cyCount] -join ' ')"
+			cmd /c "npx cypress-ntlm run --browser chrome $($args[2..$cyEnd] -join ' ')"
 		}
 	} else {
 		if ( $args[1] -eq 'open' ) {
 			if ( $args[2] -eq $null ) {
 				cmd /c "npx cypress-ntlm run --browser chrome --headed --no-exit"
 			} else {
-				cmd /c "npx cypress-ntlm run --browser chrome --headed --no-exit $($args[2..$cyCount] -join ' ')"
+				cmd /c "npx cypress-ntlm run --browser chrome --headed --no-exit $($args[2..$cyEnd] -join ' ')"
 			}
 		}
 	}
@@ -20,14 +20,14 @@ if ( $args[0] -eq 'ntlm' ) {
 		if ( $args[1] -eq $null ) {
 			cmd /c "npx cypress run --browser chrome"
 		} else {
-			cmd /c "npx cypress run --browser chrome $($args[1..$cyCount] -join ' ')"
+			cmd /c "npx cypress run --browser chrome $($args[1..$cyEnd] -join ' ')"
 		}
 	} else {
 		if ( $args[0] -eq 'open' ) {
 			if ( $args[1] -eq $null ) {
 				cmd /c "npx cypress run --browser chrome --headed --no-exit"
 			} else {
-				cmd /c "npx cypress run --browser chrome --headed --no-exit $($args[1..$cyCount] -join ' ')"
+				cmd /c "npx cypress run --browser chrome --headed --no-exit $($args[1..$cyEnd] -join ' ')"
 			}
 		}
 	}
