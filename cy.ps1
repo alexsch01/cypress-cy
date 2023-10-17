ffe2c9003c85b26f96bef634105403100096b3c6
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-17 13:11:08 -0400

    Update cy.ps1

--- cy.ps1
+++ cy.ps1
@@ -1,34 +1,18 @@
 $cyEnd = $args.Count-1
 if ( $args[0] -eq 'ntlm' ) {
 	if ( $args[1] -eq 'run' ) {
-		if ( $args[2] -eq $null ) {
-			cmd /c "npx cypress-ntlm run --browser chrome"
-		} else {
-			cmd /c "npx cypress-ntlm run --browser chrome $($args[2..$cyEnd] -join ' ')"
-		}
+		npx cypress-ntlm run --browser chrome @($args[2..$cyEnd])
 	} else {
 		if ( $args[1] -eq 'open' ) {
-			if ( $args[2] -eq $null ) {
-				cmd /c "npx cypress-ntlm run --browser chrome --headed --no-exit"
-			} else {
-				cmd /c "npx cypress-ntlm run --browser chrome --headed --no-exit $($args[2..$cyEnd] -join ' ')"
-			}
+			npx cypress-ntlm run --browser chrome --headed --no-exit @($args[2..$cyEnd])
 		}
 	}
 } else {
 	if ( $args[0] -eq 'run' ) {
-		if ( $args[1] -eq $null ) {
-			cmd /c "npx cypress run --browser chrome"
-		} else {
-			cmd /c "npx cypress run --browser chrome $($args[1..$cyEnd] -join ' ')"
-		}
+		npx cypress run --browser chrome @($args[1..$cyEnd])
 	} else {
 		if ( $args[0] -eq 'open' ) {
-			if ( $args[1] -eq $null ) {
-				cmd /c "npx cypress run --browser chrome --headed --no-exit"
-			} else {
-				cmd /c "npx cypress run --browser chrome --headed --no-exit $($args[1..$cyEnd] -join ' ')"
-			}
+			npx cypress run --browser chrome --headed --no-exit $($args[1..$cyEnd])
 		}
 	}
 }
