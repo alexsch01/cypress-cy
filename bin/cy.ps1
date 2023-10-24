352d6db5cc00b29093bb9fec0cc11194263d613e
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-24 10:50:35 -0400

    Create cy.ps1

---
+++ bin/cy.ps1
@@ -0,0 +1,14 @@
+$cyEnd = $args.Count-1
+if ( $args[0] -eq 'ntlm' ) {
+  if ( $args[1] -eq 'run' ) {
+		npx cypress-ntlm run --browser chrome @($args[2..$cyEnd])
+	} elseif ( $args[1] -eq 'open' ) {
+		npx cypress-ntlm run --browser chrome --headed --no-exit @($args[2..$cyEnd])
+	}
+} elseif ( $args[0] -eq 'run' ) {
+	npx cypress run --browser chrome @($args[1..$cyEnd])
+} elseif ( $args[0] -eq 'open' ) {
+	npx cypress run --browser chrome --headed --no-exit @($args[1..$cyEnd])
+} else {
+	npx cypress @($args)
+}
