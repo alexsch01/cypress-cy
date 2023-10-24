11cb1965d0ac2fdd6a21986554f301e96518e59f
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-24 14:47:04 -0400

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,5 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 
-fs.writeFileSync(path.resolve(__dirname, '..', '..', '..', 'cy'), fs.readFileSync(path.resolve(__dirname, '..', 'bin', 'cy')))
-fs.writeFileSync(path.resolve(__dirname, '..', '..', '..', 'cy.cmd'), fs.readFileSync(path.resolve(__dirname, '..', 'bin', 'cy.cmd')))
+if(process.platform == 'win32') {
+  fs.writeFileSync(path.resolve(__dirname, '..', '..', '..', 'cy'), fs.readFileSync(path.resolve(__dirname, '..', 'bin', 'cy')))
+  fs.writeFileSync(path.resolve(__dirname, '..', '..', '..', 'cy.cmd'), fs.readFileSync(path.resolve(__dirname, '..', 'bin', 'cy.cmd')))
+} else {
+  if(!process.env.npm_config_global) {
+    fs.writeFileSync(path.resolve(__dirname, '..', '..', '..', 'cy'), fs.readFileSync(path.resolve(__dirname, '..', 'bin', 'cy')))
+  } else {
+    fs.writeFileSync(path.resolve(__dirname, '..', '..', '..', '..', 'bin', 'cy'), fs.readFileSync(path.resolve(__dirname, '..', 'bin', 'cy')))
+  }
+}
