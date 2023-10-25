a8307a32adb7cb0cbe0a2132f6f4ee2fff02afdf
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-25 11:16:46 -0400

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,16 +4,13 @@ const { execSync } = require('child_process')
 
 const inputPathSH = path.resolve(__dirname, '..', 'bin', 'cy')
 const inputPathCMD = path.resolve(__dirname, '..', 'bin', 'cy.cmd')
-const inputPathPS1 = path.resolve(__dirname, '..', 'bin', 'cy.ps1')
 
 if(process.platform == 'win32') {
   const outputPathSH = path.resolve(__dirname, '..', '..', '..', 'cy')
   const outputPathCMD = path.resolve(__dirname, '..', '..', '..', 'cy.cmd')
-  const outputPathPS1 = path.resolve(__dirname, '..', '..', '..', 'cy.ps1')
   
   fs.writeFileSync(outputPathSH, fs.readFileSync(inputPathSH))
   fs.writeFileSync(outputPathCMD, fs.readFileSync(inputPathCMD))
-  fs.writeFileSync(outputPathPS1, fs.readFileSync(inputPathPS1))
 } else {
   const outputPathSH = path.resolve(__dirname, '..', '..', '..', '..', 'bin', 'cy')
 
