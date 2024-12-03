9c441ef3491ae8aa0a6c7a6690f5633a3e3177d9
Alex Schwartz <alexschwartz01@gmail.com>
2024-12-03 10:02:41 -0500

    remove `--browser chrome` since defaultBrowser is now a Cypress config option (#1)
    * Update cy.cmd
    
    * Update cy
    
    * Update README.md
    
    * Update package.json

--- bin/cy.cmd
+++ bin/cy.cmd
@@ -7,14 +7,14 @@ for /f "tokens=1,* delims= " %%a in ("%ALL_BUT_FIRST%") do set ALL_BUT_SECOND=%%
 
 if "%1" == "ntlm" (
 	if "%2" == "run" (
-		npx cypress-ntlm run --browser chrome %ALL_BUT_SECOND%
+		npx cypress-ntlm run %ALL_BUT_SECOND%
 	) else if "%2" == "open" (
-		npx cypress-ntlm run --browser chrome --headed --no-exit %ALL_BUT_SECOND%
+		npx cypress-ntlm run --headed --no-exit %ALL_BUT_SECOND%
 	)
 ) else if "%1" == "run" (
-	npx cypress run --browser chrome %ALL_BUT_FIRST%
+	npx cypress run %ALL_BUT_FIRST%
 ) else if "%1" == "open" (
-	npx cypress run --browser chrome --headed --no-exit %ALL_BUT_FIRST%
+	npx cypress run --headed --no-exit %ALL_BUT_FIRST%
 ) else (
 	npx cypress %*
 )
